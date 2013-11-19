944b24e2ade0c8b71dc5cea0ff37d77a4eb999af
DustinCalkins <dustin.j.calkins@gmail.com>
2013-11-19 10:09:06 -0700

    Removed scanning from main and put into parser, might be minor changes to other files

--- GeneBankCreateBTree.java
+++ GeneBankCreateBTree.java
@@ -1,66 +1,46 @@
-import java.io.File;
-import java.util.Scanner;
-
+import java.io.*;
+import java.util.*;
 
 public class GeneBankCreateBTree {
 
-	public static void main(String[] args) {
-	    
-	       boolean withCache = false;
-	       int degree = 0;
-	       int seqLength = 0;
-	       String gbkFileName = "";
-	       Cache dnaCache;
-	       
-	    
-	       try{
-	       
-	           if(args[0].equals("0")){
-	           }
-	           else if(args[0].equals("1")){
-	               withCache = true;
-	           }
-	           else{
-	               throw new RuntimeException("Error: Invalid first argument. Must be of the form <0/1(no/with Cache)>");
-	           }
-	           
-	       }
-	       catch(RuntimeException e){
-	       
-	           System.out.println();
-	           System.out.println("RuntimeException: " + e.getMessage());
-	           System.out.println();
-	           System.exit(1);
-	           
-	       }
-	       
-	       try{
-	            Scanner scan = new Scanner(new File(gbkFileName));
-	            
-	            while(scan.hasNext()){
-	    	        
-		            String a = scan.next();
-		            
-		            if(a.equals("ORIGIN")){
-		            
-		                
-		            
-		            }
-		        
-		        }
-		        scan.close();
-	        }
-	        catch(Exception e){
-	            System.out.println();
-	            System.out.println("Could not find file. Please make sure the file is in the proper directory and that you entered the filename correctly.");
-	            System.out.println();
-	            System.exit(1);
-	        }
-	        
-	        
-	           
-	       
-	       
-	    }
+    public static void main(String[] args) {
+    
+       boolean withCache = false;
+       int degree = 0;
+       int seqLength = 0;
+       String gbkFileName = "";
+       Cache dnaCache;
+       
+       
+    
+       try{
+       
+           if(args[0].equals("0")){
+           }
+           else if(args[0].equals("1")){
+               withCache = true;
+           }
+           else{
+               throw new RuntimeException("Error: Invalid first argument. Must be of the form <0/1(no/with Cache)>");
+           }
+           
+       }
+       catch(RuntimeException e){
+       
+           System.out.println();
+           System.out.println("RuntimeException: " + e.getMessage());
+           System.out.println();
+           System.exit(1);
+           
+       }
+       
+       gbkFileName = args[0];
+       
+       Parser parse = new Parser (seqLength, gbkFileName);
+       
+          
+       
+       
+    }
 
 }
--- Parser.java
+++ Parser.java
@@ -1,11 +1,46 @@
+import java.io.File;
+import java.util.Scanner;
 
+
+//not sure if we need parser class or just parse in the main method of GeneBankCreateBTree
 public class Parser {
-	
-	int seqLength = 0;
+
+    int seqLength = 0;
+    String fileName = "";
+    Scanner scan = null;
 
     //Constructor for Parser
-    public Parser(int i){
+    public Parser(int i, String a){
         seqLength = i;
+        fileName = a;
+        
+        try{
+            scan = new Scanner(new File(fileName));
+        }
+        catch(Exception e){
+            System.out.println();
+            System.out.println("Could not find file. Please make sure the file is in the proper directory and that you entered the filename correctly.");
+            System.out.println();
+            System.exit(1);
+        }
+    }
+    
+    public long nextSequence(){
+    	
+    	while(scan.hasNext()){
+    	    
+            String a = scan.next();
+            
+            if(a.equals("ORIGIN")){
+            
+                
+            
+            }
+        
+        }
+        scan.close();
+    	
+        return 5;
     }
 
 }
