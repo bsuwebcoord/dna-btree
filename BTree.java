03a5ac4ba4c4c5fe66615aa22e210f54ddb4a0c3
DustinCalkins <dustin.j.calkins@gmail.com>
2013-11-19 10:42:26 -0700

    Added bTreeSplitChild, bTreeInsert, and bTreeInsertNonfull

--- BTree.java
+++ BTree.java
@@ -2,11 +2,11 @@
 public class BTree {
 	
 	BTreeNode root = null;
+	int t;
 	
 	//similar to B-Tree-Create code in book p. 492
 	public BTree(){
 		root = new BTreeNode(5);
-		root.leaf = true;
 		root.numTreeObjects = 0;
 	}
 	
@@ -25,17 +25,82 @@ public class BTree {
 			return null;
 		}
 		else{
-			diskRead(x, Ci){
-				return bTreeSearch(x.Ci, k);
-			}
+			//I'm not sure what Ci is in the book
+			//needs to be a BTreeNode though
+			diskRead(x, Ci);
+			return bTreeSearch(x.Ci, k);
 		}
 	}
 	
 	bTreeSplitChild(BTreeNode x, int i){
 		BTreeNode z = new BTreeNode(5);
-		y 
+		y = x.Ci;
+		z.leaf = y.leaf;
+		z.numTreeObjects = t - 1;
+		for(int j = 1; j <= t-1; j++){
+			z.treeO[j].key = y.treeO[j+t].key;
+		}
+		if(!y.leaf){
+			for(int j = 1; j <= t; j++){
+				z.Cj = y.C(j+t)
+			}
+		}
+		y.numTreeObjects = t - 1;
+		for(int j = x.numTreeObjects + 1; j >= i+1; j--){
+			x.C(j+1) = x.Cj
+		}
+		x.C(i+1) = z;
+		for(int j = x.numTreeObjects; j >= i; j--){
+			x.treeO[j+1].key = x.treeO[j].key;
+		}
+		x.treeO[i].key = y.treeO[t].key;
+		x.numTreeObjects = x.numTreeObjects + 1;
+		diskWrite(y);
+		diskWrite(z);
+		diskWrite(x);
 	}
 	
+	bTreeInsert(long k){
+		BTreeNode r = root;
+		if(root.numTreeObjects == 2t-1){
+			BTreeNode s = new BTreeNode(5);
+			root = s;
+			s.leaf = false;
+			s.numTreeObjects = 0;
+			s.C1 = r;
+			bTreeSplitChild(s, 1);
+			bTreeInsertNonfull(s,k);
+		}
+		else{
+			bTreeInsertNonfull(r,k);
+		}
+	}
 	
+	bTreeInsertNonfull(BTreeNode x, long k){
+		int i = x.numTreeObjects;
+		if(x.leaf){
+			while(i >= 1 && k < x.treeO[i].key){
+				x.treeO[i+1].key = x.treeO[i].key;
+				i--;
+			}
+			x.treeO[i+1].key = k;
+			x.numTreeObjects = x.numTreeObjects + 1;
+			diskWrite(x);
+		}
+		else{
+			while(i >= 1 && k < x.treeO[i].key){
+				i--;
+			}
+			i++;
+			diskRead(x.Ci)
+			if(x.Ci.numTreeObjects == 2t-1){
+				bTreeSplitChild(x, i);
+				if(k > x.treeO[i].key){
+					i++;
+				}
+			}
+			bTreeInsertNonfull(x.Ci, k);
+		}
+	}
 
 }
