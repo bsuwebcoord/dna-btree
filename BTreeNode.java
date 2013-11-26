9d6d0e72d6435548fa4e525a59aa9a33e74dda6d
DustinCalkins <dustin.j.calkins@gmail.com>
2013-11-26 14:56:35 -0700

    Update BTreeNode.java

--- BTreeNode.java
+++ BTreeNode.java
@@ -1,10 +1,11 @@
-
 public class BTreeNode {
 
     TreeObject[] treeO;
     int numTreeObjects;
     int[] childPointers;
     int parentPointer;
+    int globalOffset;
+    boolean leaf;
     
     //Constructor for BTreeNode
     public BTreeNode(int i){
