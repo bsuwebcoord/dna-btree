589d4e5bebfe49335b586a342e675ab2ed59c111
DustinCalkins <dustin.j.calkins@gmail.com>
2013-12-02 11:50:41 -0700

    Update GeneBankCreateBTree.java

--- GeneBankCreateBTree.java
+++ GeneBankCreateBTree.java
@@ -17,7 +17,7 @@ public class GeneBankCreateBTree {
        //Error handling and setting values from user input:
        
        if(args.length < 4 || args.length > 6){
-    	   System.out.println();
+               System.out.println();
            System.out.println("You provided too few or too many arguments.");
            System.out.println("Arguments have the following form: <0/1(no/with Cache)> <degree> <gbk file> <sequence length> [<cache size>] [<debug level>]");
            System.out.println();
@@ -31,7 +31,7 @@ public class GeneBankCreateBTree {
            else if(args[0].equals("1")){
                withCache = true;
                if(args.length < 4){
-            	   System.out.println();
+                       System.out.println();
                    System.out.println("You provided too few arguments.");
                    System.out.println("Since you specified using a cache, you must provide a fourth argument of the following form: <cache size>");
                    System.out.println();
@@ -55,13 +55,13 @@ public class GeneBankCreateBTree {
        try{
            
            if(args[1].equals("0")){
-        	   //default to optimal degree, do nothing since it's already set at optimal
+                   //default to optimal degree, do nothing since it's already set at optimal
            }
-           else if(Integer.parseInt(args[1]) > 0 && Integer.parseInt(args[1]) <= 127){
-        	   degree = Integer.parseInt(args[1]);
+           else if(Integer.parseInt(args[1]) > 0){
+                   degree = Integer.parseInt(args[1]);
            }
            else{
-               throw new RuntimeException("Error: Invalid second argument. Must be of the form <degree>, where the degree is greater than 0 and less than or equal to 127.");
+               throw new RuntimeException("Error: Invalid second argument. Must be of the form <degree>, where the degree is greater than 0.");
            }
            
        }
@@ -80,7 +80,7 @@ public class GeneBankCreateBTree {
        try{
            
            if(Integer.parseInt(args[3]) > 0 && Integer.parseInt(args[3]) <= 31){
-        	   seqLength = Integer.parseInt(args[3]);
+                   seqLength = Integer.parseInt(args[3]);
            }
            else{
                throw new RuntimeException("Error: Invalid fourth argument. Must be of the form <sequence length>, where the sequence length is greater than 0 and less than or equal to 31.");
@@ -98,10 +98,10 @@ public class GeneBankCreateBTree {
        
        //if a cache was specified, check that the fifth argument is in the correct form
        if(args[0].equals("1")){
-    	   
-    	   try{
+               
+               try{
                if(Integer.parseInt(args[4]) > 0){
-            	   dnaCache = new Cache(Integer.parseInt(args[4]));
+                       dnaCache = new Cache(Integer.parseInt(args[4]));
                }
                else{
                    throw new RuntimeException("Error: Invalid fifth argument. Must be of the form <cache size>, where the cache size is greater than 0.");
@@ -116,82 +116,82 @@ public class GeneBankCreateBTree {
                System.exit(1);
                
            }
-    	   
-    	   if(args.length == 6){
-    		   try{
-    	           
-    	           if(args[5].equals("0")){
-    	        	   //do nothing since debug level is already set to 0 by default
-    	           }
-    	           else if(args[5].equals("1")){
-    	        	   debugLevel = 1;
-    	           }
-    	           else{
-    	               throw new RuntimeException("Error: Invalid sixth argument. Must be of the form <debug level>, where the debug level is 0  or 1.");
-    	           }
-    	           
-    	       }
-    	       catch(RuntimeException e){
-    	       
-    	           System.out.println();
-    	           System.out.println("RuntimeException: " + e.getMessage());
-    	           System.out.println();
-    	           System.exit(1);
-    	           
-    	       }
-    	   }
-    	   
+               
+               if(args.length == 6){
+                       try{
+                       
+                       if(args[5].equals("0")){
+                               //do nothing since debug level is already set to 0 by default
+                       }
+                       else if(args[5].equals("1")){
+                               debugLevel = 1;
+                       }
+                       else{
+                           throw new RuntimeException("Error: Invalid sixth argument. Must be of the form <debug level>, where the debug level is 0  or 1.");
+                       }
+                       
+                   }
+                   catch(RuntimeException e){
+                   
+                       System.out.println();
+                       System.out.println("RuntimeException: " + e.getMessage());
+                       System.out.println();
+                       System.exit(1);
+                       
+                   }
+               }
+               
        }
        else if(args.length == 5){
-    	   try{
-	           
-	           if(args[4].equals("0")){
-	        	   //do nothing since debug level is already set to 0 by default
-	           }
-	           else if(args[4].equals("1")){
-	        	   debugLevel = 1;
-	           }
-	           else{
-	               throw new RuntimeException("Error: Invalid fifth argument. Must be of the form <debug level>, where the debug level is 0  or 1.");
-	           }
-	           
-	       }
-	       catch(RuntimeException e){
-	       
-	           System.out.println();
-	           System.out.println("RuntimeException: " + e.getMessage());
-	           System.out.println();
-	           System.exit(1);
-	           
-	       }
+               try{
+                   
+                   if(args[4].equals("0")){
+                           //do nothing since debug level is already set to 0 by default
+                   }
+                   else if(args[4].equals("1")){
+                           debugLevel = 1;
+                   }
+                   else{
+                       throw new RuntimeException("Error: Invalid fifth argument. Must be of the form <debug level>, where the debug level is 0  or 1.");
+                   }
+                   
+               }
+               catch(RuntimeException e){
+               
+                   System.out.println();
+                   System.out.println("RuntimeException: " + e.getMessage());
+                   System.out.println();
+                   System.exit(1);
+                   
+               }
        }
        
        tree = new BTree(degree);
        
        tree.childrenInitializer = new int[2*tree.t];
        tree.treeObjectInitializer = new TreeObject[(2*tree.t)-1];
-	
-		//initialize the child array and tree object array to a constant size in order to read correctly
-		for(int i = 0; i < tree.treeObjectInitializer.length; i++){
-			tree.childrenInitializer[i] = 0;
-			tree.treeObjectInitializer[i] = new TreeObject(0,0);
-		}
-		//initialize the last item in the child array that wasn't covered by the for loop above
-		tree.childrenInitializer[tree.childrenInitializer.length-1] = 0;
-	
-		tree.root = new BTreeNode(0, true, 0, 0, tree.childrenInitializer, tree.treeObjectInitializer);
+        
+                //initialize the child array and tree object array to a constant size in order to read correctly
+                for(int i = 0; i < tree.treeObjectInitializer.length; i++){
+                        tree.childrenInitializer[i] = 0;
+                        tree.treeObjectInitializer[i] = new TreeObject(0,0);
+                }
+                //initialize the last item in the child array that wasn't covered by the for loop above
+                tree.childrenInitializer[tree.childrenInitializer.length-1] = 0;
+        
+                tree.root = new BTreeNode(0, true, 0, 0, tree.childrenInitializer, tree.treeObjectInitializer);
        
        try{
               
-    	   tree.dis = new RandomAccessFile("gbkfile.bin", "rw");
+               tree.dis = new RandomAccessFile("gbkfile.bin", "rw");
                
        }
        catch(FileNotFoundException e){
                
-       		System.out.println();
-       		System.out.println("RuntimeException: " + e.getMessage());
-       		System.out.println();
-       		System.exit(1);
+                       System.out.println();
+                       System.out.println("RuntimeException: " + e.getMessage());
+                       System.out.println();
+                       System.exit(1);
                
        }
        
@@ -206,19 +206,20 @@ public class GeneBankCreateBTree {
        long foundKeyNodeGlobalPosition = 0;
        
        //insert or update all subsequences from the gbk file until the end of the file is reached
+       
        while(binarySequence != -1){
-    	   
-    	   foundKeyNodeGlobalPosition = tree.bTreeSearch(tree.root, binarySequence);
-    	   
-    	   //if the key wasn't found, insert it into the BTree
-    	   if(foundKeyNodeGlobalPosition == -1){
-    		   tree.bTreeInsert(binarySequence);
-    	   }
-    	   //if the key was found, update the node with an increased frequency for that key and write the updated node to disk
-    	   else{
-    		   BTreeNode updatedNode = tree.diskRead(foundKeyNodeGlobalPosition);
-    		   
-    		   int i = 0;
+               
+               foundKeyNodeGlobalPosition = tree.bTreeSearch(tree.root, binarySequence);
+               
+               //if the key wasn't found, insert it into the BTree
+               if(foundKeyNodeGlobalPosition == -1){
+                       tree.bTreeInsert(binarySequence);
+               }
+               //if the key was found, update the node with an increased frequency for that key and write the updated node to disk
+               else{
+                       BTreeNode updatedNode = tree.diskRead(foundKeyNodeGlobalPosition);
+                       
+                       int i = 0;
                
                while(binarySequence != updatedNode.treeO[i].key){
                        i++;
@@ -227,12 +228,30 @@ public class GeneBankCreateBTree {
                
                tree.diskWrite(updatedNode.globalOffset, updatedNode);
                
-    	   }
-    	   
-    	   binarySequence = parse.nextBinSequence();
+               }
+               
+               binarySequence = parse.nextBinSequence();
        }
        
+       /*
+       System.out.printf("\nThe size of the bin file before anything is written: %d\n", tree.dis.length());
        
+       //tree.diskWrite(-1, tree.root);
+       //tree.diskWrite(-1, tree.root);
+       //BTreeNode test = tree.diskRead(0);
+       
+       tree.dis.writeInt(5);
+       tree.dis.seek(tree.dis.length());
+       tree.dis.writeInt(2147483647);
+       
+       System.out.printf("\nThe size of the bin file after int is written: %d\n", tree.dis.length());
+       
+       tree.dis.seek(0);
+       
+       
+       System.out.println(tree.dis.readInt());
+       System.out.println(tree.dis.readInt());
+       */
        
     }
 
